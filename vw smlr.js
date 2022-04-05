7fc97ee5f73764e3d3c040bf4084e8722b1042bc
DEVENSHRAJ <69020033+DEVENSHRAJ@users.noreply.github.com>
2022-04-06 01:20:20 +0530

    basic code

---
+++ vw smlr.js
@@ -0,0 +1,11 @@
+var coll = document.getElementsByClassName("collapsible");
+
+coll[0].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var content = this.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+});
